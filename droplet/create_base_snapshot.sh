940b42a24261d3b58eb6c29e79b6fdec652c7ec5
Marcus Böhm <wif.mboehm@gmx.de>
2018-07-16 11:21:29 +0200

    also upload benchmarking script

--- droplet/create_base_snapshot.sh
+++ droplet/create_base_snapshot.sh
@@ -3,7 +3,7 @@ set -e
 ID=$1
 if [ -z "${ID}" ]; then
     echo "No Droplet ID provided. Will create a new Droplet."
-    ID=`doctl compute droplet create mbtest --no-header --format "ID" --wait --size 1gb --region fra1 --image 30970148 --ssh-keys f9:60:c8:4e:27:83:ec:59:b9:bb:a6:20:9f:2e:2d:3b`
+    ID=`doctl compute droplet create perf-test-base-snapshot --no-header --format "ID" --wait --size 1gb --region fra1 --image 30970148 --ssh-keys f9:60:c8:4e:27:83:ec:59:b9:bb:a6:20:9f:2e:2d:3b`
 fi
 echo "ID is $ID"
 IP=`doctl compute droplet get $ID --no-header --format="PublicIPv4"`
@@ -15,6 +15,8 @@ echo "Uploading import data"
 scp -rp ../import_data root@$IP:/root/
 echo "Uploading the queries to be tested"
 scp -rp ../querycollection root@$IP:/root/
+echo "Uploading the benchmark script"
+scp -rp ../benchmark root@$IP:/root/
 echo "Installing the Tools"
 ssh root@$IP 'cd setup_scripts && ./setup.sh'
 echo "Deleting old snapshot" # we are looping because there might be none and we do not want to error in this case
