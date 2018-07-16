2867892a36f331a7ee434f568c8e8fd43b5a2567
Marcus Böhm <wif.mboehm@gmx.de>
2018-07-16 17:52:44 +0200

    factor yarn installation into new script

--- setup_scripts/tools/setup.sh
+++ setup_scripts/tools/setup.sh
@@ -1,7 +1,5 @@
 ./install_vegeta.sh
 ./install_node.sh
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+./install_yarn.sh
 yarn global add prisma
 ./increase_file_descriptor_limit.sh
