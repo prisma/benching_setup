3eb4facab9a12f2821bd70e1cbadd36b24a0f540
Marcus Böhm <wif.mboehm@gmx.de>
2018-07-18 15:37:11 +0200

    adapt bench on droplet script for use with our company account

--- droplet/benchmark_on_droplet.sh
+++ droplet/benchmark_on_droplet.sh
@@ -8,25 +8,26 @@ IMPORT_FILE=$3
 #     echo "No Droplet ID provided. Will create a new Droplet."
 #     ID=`doctl compute droplet create perf-test-$CONNECTOR --no-header --format "ID" --wait --size 1gb --region fra1 --image 30970148 --ssh-keys f9:60:c8:4e:27:83:ec:59:b9:bb:a6:20:9f:2e:2d:3b`
 # fi
-ID=`doctl compute droplet create perf-test-$CONNECTOR --no-header --format "ID" --wait --size 1gb --region fra1 --image 30970148 --ssh-keys f9:60:c8:4e:27:83:ec:59:b9:bb:a6:20:9f:2e:2d:3b`
+ID=`doctl compute droplet create perf-test-$CONNECTOR --no-header --format "ID" --wait --size 8gb --region fra1 --image 30970148 --ssh-keys 1b:7d:9e:eb:81:55:33:71:94:9d:37:f2:ea:c9:8a:99`
 echo "ID is $ID"
 IP=`doctl compute droplet get $ID --no-header --format="PublicIPv4"`
 echo "IP is $IP"
-until ssh root@$IP exit
+until ssh -i $HOME/.ssh/prisma_digital_ocean root@$IP exit
 do
   echo "The Droplet does not accept SSH connections yet. Will try again in 1 second."
   sleep 1;
 done
 echo "Uploading the required setup script"
-rsync -av --progress ../setup_scripts/prisma-server root@$IP:.
+# rsync -i $HOME/.ssh/prisma_digital_ocean -av --progress ../setup_scripts/prisma-server root@$IP:.
+scp -r -i $HOME/.ssh/prisma_digital_ocean ../setup_scripts/prisma-server root@$IP:.
 echo "Starting the Prisma Server"
-ssh root@$IP "cd prisma-server && ./start.sh $CONNECTOR $PRISMA_VERSION"
+ssh -i $HOME/.ssh/prisma_digital_ocean root@$IP "cd prisma-server && ./start.sh $CONNECTOR $PRISMA_VERSION"
 if [ -n "${IMPORT_FILE}" ]; then
     echo "Resetting the data of the Prisma server"
-    ssh root@$IP "docker run --net=host prismagraphql/benchmarks:latest reset-server $IMPORT_FILE"  
+    ssh -i $HOME/.ssh/prisma_digital_ocean root@$IP "docker run --net=host prismagraphql/benchmarks:latest reset-server $IMPORT_FILE"  
 fi
 echo "Starting the Benchmark..."
-ssh root@$IP "docker run --net=host prismagraphql/benchmarks:latest bench-server $CONNECTOR"  
+ssh -i $HOME/.ssh/prisma_digital_ocean root@$IP "docker run --net=host prismagraphql/benchmarks:latest bench-server $CONNECTOR"  
 
 echo "Deleting Droplet"
 doctl compute droplet delete --force $ID
