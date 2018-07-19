37c6adf63f358539503ec7353c823abe3e49147e
Marcus Böhm <wif.mboehm@gmx.de>
2018-07-19 18:27:24 +0200

    fix delete droplet script

--- droplet/delete_this_droplet.sh
+++ droplet/delete_this_droplet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TOKEN="${1:?Provide the Digital Ocean API Token}"
 echo "Deleting this Droplet!"
-DROPLET_ID=`curl http://169.254.169.254/metadata/v1/tags/id`
+DROPLET_ID=`curl http://169.254.169.254/metadata/v1/id`
 curl -X DELETE -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets/$DROPLET_ID"
 shutdown -h now
