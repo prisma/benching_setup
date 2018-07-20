566e8e73b6e07a3ef3598c126a65ca505022128c
Marcus Böhm <wif.mboehm@gmx.de>
2018-07-20 17:28:26 +0200

    wait until prisma has started instead of stupid sleep

--- setup_scripts/prisma-server/start.sh
+++ setup_scripts/prisma-server/start.sh
@@ -3,4 +3,9 @@ CONNECTOR="${1:?Provide the connector you want to use for this test run}"
 export PRISMA_VERSION="${2:?Provide the Prisma version you want to use for this test run}"
 echo "Starting the Prisma server with version $PRISMA_VERSION"
 docker-compose -f docker-compose.$CONNECTOR.yml up -d
-sleep 30
+echo "Waiting until Prisma has finished booting"
+until curl -s 'http://localhost:4466' > /dev/null
+do
+  echo "The prisma server has not booted yet. Will try again in 1 second."
+  sleep 1;
+done
