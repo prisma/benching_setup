21f24f7715357d578c589b8d0537daf7617d7a14
Marcus Böhm <wif.mboehm@gmx.de>
2018-07-20 14:14:50 +0200

    make delete more resilient

--- droplet/bench.sh
+++ droplet/bench.sh
@@ -11,11 +11,12 @@ VERSION=`getValue version | tr _ .`
 API_TOKEN=`getValue api_token`
 TEST=`getValue test`
 echo "Starting benchmark for Connector $CONNECTOR with version $VERSION based on import file $IMPORT_FILE"
+CURRENT_DIR=`pwd`
 cd ../setup_scripts/prisma-server && ./start.sh $CONNECTOR $VERSION && cd -
-docker run --net=host prismagraphql/benchmarks:latest reset-server $IMPORT_FILE
-docker run --net=host prismagraphql/benchmarks:latest bench-server $CONNECTOR $TEST
+docker run -it --net=host prismagraphql/benchmarks:latest reset-server $IMPORT_FILE
+docker run -it --net=host prismagraphql/benchmarks:latest bench-server $CONNECTOR $TEST
 
 if [ -n "${API_TOKEN}" ]; then
   pwd
-  $HOME/benching_setup/droplet/delete_this_droplet.sh $API_TOKEN
+  $CURRENT_DIR/delete_this_droplet.sh $API_TOKEN
 fi
