2fc5cd144f93b832351831566e758d9c31c469e7
Marcus Böhm <wif.mboehm@gmx.de>
2018-12-17 18:14:34 +0100

    add mongo dumps

--- scripts/bin.ts
+++ scripts/bin.ts
@@ -1,8 +1,8 @@
-import { fill } from './fill'
+import { fill } from "./fill";
 
 fill({
-  endpoint: 'http://localhost:4466',
-  totalCount: 5000,
+  endpoint: "http://localhost:4466",
+  totalCount: 1000,
   concurrency: 10,
-  batchSize: 1,
-})
+  batchSize: 1
+});
