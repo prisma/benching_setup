d23b00ef9e843c4cd9670f7dda4884d21d4050f4
Julius de Bruijn <bruijn@prisma.io>
2019-09-23 18:31:46 +0200

    Directory structure for prisma2

--- setup_scripts/prisma-server/start.sh
+++ setup_scripts/prisma-server/start.sh
@@ -2,9 +2,8 @@
 
 set -e
 CONNECTOR="${1:?Provide the connector you want to use for this test run}"
-export PRISMA_VERSION="${2:?Provide the Prisma version you want to use for this test run}"
 
-echo "Starting the Prisma server with version $PRISMA_VERSION"
+echo "Starting the Prisma server"
 docker-compose -f docker-compose.$CONNECTOR.yml up -d
 
 echo "Waiting until Prisma has finished booting"
